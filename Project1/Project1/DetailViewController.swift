2783720395aed902531664ecb50da3351ee65078
demirdev <demorapps@gmail.com>
2023-07-16 15:59:27 +0300

    add Project 1: Day 16, 17, 18

---
+++ Project1/Project1/DetailViewController.swift
@@ -0,0 +1,48 @@
+//
+//  DetailViewController.swift
+//  Project1
+//
+//  Created by Mehmet Demir on 16.07.2023.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+    @IBOutlet var imageView: UIImageView!
+    var selectedImage: String?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        navigationItem.largeTitleDisplayMode = .never
+        
+        title = selectedImage
+        
+        if let selectedImage = selectedImage {
+            imageView.image = UIImage(named: selectedImage)
+        }
+
+        // Do any additional setup after loading the view.
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        navigationController?.hidesBarsOnTap = true
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewDidDisappear(animated)
+        navigationController?.hidesBarsOnTap = false
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Project1/Project1/ViewController.swift
@@ -0,0 +1,56 @@
+//
+//  ViewController.swift
+//  Project1
+//
+//  Created by Mehmet Demir on 15.07.2023.
+//
+
+import UIKit
+
+class ViewController: UITableViewController {
+    var pictures = [String]()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        navigationController?.navigationBar.prefersLargeTitles = true
+        
+        title = "Storm Viewer"
+        
+        let fm = FileManager.default
+        let path = Bundle.main.resourcePath!
+        let items = try! fm.contentsOfDirectory(atPath: path)
+        
+        for item in items {
+            if item.hasPrefix("unnamed") {
+                pictures.append(item)
+            }
+        }
+        pictures = pictures.sorted()
+        print(pictures)
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        if let vc = storyboard?.instantiateViewController(withIdentifier: "Detail") as? DetailViewController {
+            vc.selectedImage = pictures[indexPath.row]
+            navigationController?.pushViewController(vc, animated: true)
+        }
+    }
+    
+    
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        1
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        pictures.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Picture", for: indexPath)
+        cell.textLabel?.text = pictures[indexPath.row]
+        return cell
+    }
+
+
+}
+
