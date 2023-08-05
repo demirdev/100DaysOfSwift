5989660939c6af6b364cf89baa9396190fda0610
demirdev <demorapps@gmail.com>
2023-08-05 11:04:00 +0300

    add Day 28 - Project 5 - Word Scramble, UITextChecker

--- Project 5 - Word Scramble/Project 5 - Word Scramble/ViewController.swift
+++ Project 5 - Word Scramble/Project 5 - Word Scramble/ViewController.swift
@@ -34,14 +34,70 @@ class ViewController: UITableViewController {
     }
 
     func submit(_ answer: String) {
-        print(answer)
+        let lowerAnswer = answer.lowercased()
         
-        if(allWords.contains(answer)){
-            usedWords.append(answer)
-            
-            tableView.reloadData()
+        let errorTitle: String
+        let errorMessage: String
+        
+        if isPossible(word: lowerAnswer) {
+            if isOriginal(word: lowerAnswer) {
+                if isReal(word: lowerAnswer){
+                    usedWords.insert(lowerAnswer, at: 0)
+                    
+                    let indexPath = IndexPath(row: 0, section: 0)
+                    tableView.insertRows(at: [indexPath], with: .automatic)
+                    
+                    return
+                } else {
+                    errorTitle = "Word not recognized"
+                    errorMessage = "You can't just make them up, you know!"
+                }
+            } else {
+                errorTitle = "Word already userd"
+                errorMessage = "Be more original!"
+            }
+        } else {
+            errorTitle = "Word not possible"
+            errorMessage = "You can't spell that word from (\(title!.lowercased()))."
         }
         
+        let ac = UIAlertController(title: errorTitle, message: errorMessage, preferredStyle: .alert)
+        
+        ac.addAction(UIAlertAction(title: "OK", style: .default))
+        
+        present(ac, animated: true)
+        
+        
+        
+        
+    }
+    
+    func isPossible(word: String) -> Bool {
+        guard var tempWord = title?.lowercased() else { return false }
+        
+        for letter in word {
+            if let position = tempWord.firstIndex(of: letter) {
+                tempWord.remove(at: position)
+            } else {
+                return false
+            }
+        }
+        
+        return true
+    }
+    
+    func isOriginal(word: String) -> Bool {
+        return !usedWords.contains(word)
+    }
+    
+    func isReal(word: String) -> Bool {
+        let checker = UITextChecker()
+        /// utf16 for UITextChecker (written in objc)
+        /// word, may be contains emojis : UTF-16
+        let range = NSRange(location: 0, length: word.utf16.count)
+        let misspelledRange = checker.rangeOfMisspelledWord(in: word, range: range, startingAt: 0, wrap: false, language: "en")
+        
+        return misspelledRange.location == NSNotFound
     }
     
     func loadWords() {
